f03dd73b828225fdf0131284cd9b8f3f9b04f22f
Matilda Mared <matildamared@live.se>
2022-07-17 20:53:32 +0200

    add form submit handler

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -3,8 +3,18 @@ import React, { useState } from "react";
 const GuessInput = () => {
 	const [guess, setGuess] = useState("");
 
+	const makeGuess = () => {
+		if (guess.length !== 5) {
+			alert("Please enter a 5-letter word.");
+			return;
+		}
+
+		console.log(`Guess: ${guess}`);
+		setGuess("");
+	};
+
 	return (
-		<form className="guess-input-wrapper">
+		<form className="guess-input-wrapper" onSubmit={makeGuess}>
 			<label htmlFor="guess-input">Enter guess:</label>
 			<input
 				id="guess-input"
