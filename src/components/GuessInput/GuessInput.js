a4a4e53e558aa41c44508320df0eef6f83e7195b
Matilda Mared <matildamared@live.se>
2022-07-17 20:51:19 +0200

    control the input with react

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 const GuessInput = () => {
+	const [guess, setGuess] = useState("");
+
 	return (
-		<form class="guess-input-wrapper">
-			<label for="guess-input">Enter guess:</label>
-			<input id="guess-input" type="text" />
+		<form className="guess-input-wrapper">
+			<label htmlFor="guess-input">Enter guess:</label>
+			<input
+				id="guess-input"
+				type="text"
+				value={guess}
+				onChange={(e) => setGuess(e.target.value.toUpperCase())}
+			/>
 		</form>
 	);
 };
