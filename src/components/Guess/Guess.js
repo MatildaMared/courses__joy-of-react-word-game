c87da2ec48b9cb85ba8a8bc1b0b1118d82b56bf4
Matilda Mared <matildamared@live.se>
2022-07-19 06:41:22 +0200

    refactor

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -3,7 +3,9 @@ import { range } from "../../utils";
 import { checkGuess } from "../../game-helpers";
 
 function Cell({ letter, status }) {
-	return <span className={`cell ${status}`}>{letter}</span>;
+	const className = status ? `cell ${status}` : "cell";
+
+	return <span className={className}>{letter}</span>;
 }
 
 function Guess({ guess, answer }) {
